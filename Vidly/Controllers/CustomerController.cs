6bac7cb319dc5bd8cca86e4baa93221cfbbdeb5c
Trevor J. Blackman <blackmtj@transcertain.com>
2019-09-24 11:58:38 -0600

    Basic Routing and Pages
    Added a page for customers and customer details, and also movies. Everything is still hard coded.

---
+++ Vidly/Controllers/CustomerController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Vidly.Models;
+
+namespace Vidly.Controllers
+{
+	public class CustomersController : Controller
+	{
+		[Route("customers/")]
+		public ActionResult Index()
+		{
+			var customers = new List<Customer>()
+			{
+				new Customer() { Name = "Trevor", Id = 1 },
+				new Customer() { Name = "Declan", Id = 2 },
+				new Customer() { Name = "Elcie", Id = 3 }
+			};
+
+			return View(customers);
+		}
+		[Route("customers/details/{Id}")]
+		public ActionResult Details(int Id)
+		{
+			var customers = new List<Customer>()
+			{
+				new Customer() { Name = "Trevor", Id = 1 },
+				new Customer() { Name = "Declan", Id = 2 },
+				new Customer() { Name = "Elcie", Id = 3 }
+			};
+			var customer = customers.Find(i => i.Id == Id); 
+
+			return View(customer);
+		}
+	}
+}
